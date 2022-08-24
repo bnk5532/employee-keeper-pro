7580ac959208fe63e8827ae92a1354e66373a8cb
Brian King <bnk5532@gmail.com>
2022-08-23 22:49:33 -0400

    code refactor and added empty functions

--- index.js
+++ index.js
@@ -1,142 +1,87 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generateMarkdown = require("./utils/generateMarkdown");
-// const employee = employee(first_name, last_name)
-// const role = role(id, title, department, salary)
-// const department = department(id, department)
-const db = require("./db/connection")
+const db = require("./db/connection");
 require("console.table");
 
-
-// function startView(){;
-inquirer.prompt([
+// function startView(){}
+inquirer
+  .prompt([
     {
-        type: "list",
-        message: "What would you like to do?",
-        name: "start",
-        choices: [
-            {value:"viewEmployee", name: "View Employee" },
-            {value: "viewDepartments", name: "View All Departments"}, 
-            {value: "viewRoles", name: "View All Roles"},
-            "Add Employee",
-            "Add Department", 
-            "Add Role", 
-            "Update Employee Role", 
-            {value: "reset", name: "Quit"},
-]},
-
-]).then(res => {
-    switch (res.start){
-        case "viewEmployee" : viewEmployee();
+      type: "list",
+      message: "What would you like to do?",
+      name: "start",
+      choices: [
+        { value: "viewEmployee", name: "View Employee" },
+        { value: "viewDepartments", name: "View All Departments" },
+        { value: "viewRoles", name: "View All Roles" },
+        { value: "addEmployee", name: "Add Employee"},
+        { value: "updateEmployee", name: "Update Employee Role"},
+        { value: "addDepartment", name: "Add Department"},
+        { value: "addRole", name: "Add Role"},
+        { value: "reset", name: "Quit" },
+      ],
+    },
+  ])
+  .then((res) => {
+    switch (res.start) {
+      case "viewEmployee":
+        viewEmployee();
         break;
-        case "viewDepartments" : viewDepartments();
+      case "viewDepartments":
+        viewDepartments();
         break;
-        case "viewRoles" : viewRoles();
+      case "viewRoles":
+        viewRoles();
         break;
-        case "reset" : reset();
+      case "addEmployee":
+        addEmployee();
         break;
-        
+      case "updateEmployee":
+        updateEmployee();
+        break;
+      case "addDepartment":
+        addDepartment();
+        break;
+      case "addRole":
+        addRole();
+        break;
+      case "reset":
+        reset();
+        break;
     }
-    console.log(res)
-}
-);
+    console.log(res);
+  });
 
 function viewEmployee() {
-    db.query("SELECT * FROM employee LEFT JOIN role on employee.role_id=role.id LEFT JOIN department on role.department_id=department.id" , (err, data) => {
-        if(err) console.log(err)
-        console.table(data)
-    })
-};
+  db.query(
+    "SELECT * FROM employee LEFT JOIN role on employee.role_id=role.id LEFT JOIN department on role.department_id=department.id",
+    (err, data) => {
+      if (err) console.log(err);
+      console.table(data);
+    }
+  );
+}
 
 function viewDepartments() {
-    db.query("SELECT * FROM department", (err, data) => {
-        if(err) console.log(err)
-        console.table(data)
-    })
-};
+  db.query("SELECT * FROM department", (err, data) => {
+    if (err) console.log(err);
+    console.table(data);
+  });
+}
 
 function viewRoles() {
-    db.query("SELECT * FROM role", (err, data) => {
-        if(err) console.log(err)
-        console.table(data)
-    })
-};
+  db.query("SELECT * FROM role", (err, data) => {
+    if (err) console.log(err);
+    console.table(data);
+  });
+}
+
+function addEmployee(){}
+function updateEmployee(){}
+function addDepartment(){}
+function addRole(){}
+
 
 function reset() {
-    startView();
-    
-};
+  startView();
+}
 
-
-
-    //prints table for each respectively
-    //SELECT * FROM employees
-    //SELECT * FROM roles
-    //SELECT * FROM departments
-    
-    // {
-    //     type: "list",
-    //     message: "View All Employees",
-    //     choices: `${employee}`
-    // },
-    // {
-    //     type: "list",
-    //     message: "View All Roles",
-    //     choices: `${role}`
-    // },
-    // {
-    //     type: "list",
-    //     message: "View All Departments",
-    //     choices: `${department}`
-    // },
-
-
-// viewEmployee(){
-
-
-
-
-
-
-
-//Add
-// (function addDepartment() {
-//         inquirer.prompt([
-//             {
-//                 type: "input",
-//                 name: "department",
-//                 message: "Department Name?"
-//             }
-        
-//         ])
-//     })
-
-
-// (function addEmployee() {
-//         inquirer.prompt([
-//             {
-//                 type: "input",
-//                 name: "first",
-//                 message: "First Name?",
-//             },
-//             {
-//                 type: "input",
-//                 name: "last",
-//                 message: "Last Name?",
-//             },
-//             {
-//                 type: "input",
-//                 message: "Email?",
-//                 name: "email"
-//             }
-//     ]) .then(({name, id, email})=> {
-//         const newManager = new Employee (
-//             name, 
-//             id, 
-//             email, 
-//         )
-//         employees.push(newManager)
-//         console.log(employees)
-//         roleList()
-//     })
-// })()
